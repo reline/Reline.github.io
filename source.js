78a6b33cd8f53523ae55e744baa076c39b2f4319
mukondono <nathanielreline@gmail.com>
2015-12-17 19:40:52 -0700

    Integrated GitHub RSSFeed

--- source.js
+++ source.js
@@ -1,3 +1,20 @@
-function underconstruction() {
-    $("body").load("underconstruction.html");
-}
+$(document).ready(function() {
+    $("body").load("underconstruction.html")
+});
+
+$(document).ready(function() {
+    $.ajax({
+        type: "GET",
+        url: "http://cors.io/?u=https://github.com/Reline.atom",
+        // dataType: "html",
+        cache: false,
+        success: function(result) {
+            console.log("Reline.atom loaded");
+
+            var xmlString = $("<span />", { html: result }).text();
+
+            $("#githubActivity").html(xmlString);
+        },
+        async: true
+    })
+});
