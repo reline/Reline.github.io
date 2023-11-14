bf27e0a1bb042b6f1dc45267f4b839d67595f9aa
Nathan Reline <nathanielreline@gmail.com>
2023-11-14 01:24:35 -0700

    Use text pointer where applicable

--- src/wasmJsMain/kotlin/Blurb.kt
+++ src/wasmJsMain/kotlin/Blurb.kt
@@ -1,8 +1,8 @@
-import androidx.compose.foundation.layout.IntrinsicSize
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 
@@ -12,5 +12,6 @@ fun Blurb() {
         "Hey, my name's Nathan! I'm an Android Developer in the Phoenix metro area. I love spending time with my four cats, wrenching on cars, and finding myself in new places eating new food.",
         fontSize = 16.sp,
         textAlign = TextAlign.Start,
+        modifier = Modifier.pointerHoverIcon(PointerIcon.Text),
     )
 }
--- src/wasmJsMain/kotlin/Header.kt
+++ src/wasmJsMain/kotlin/Header.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -17,8 +19,18 @@ fun Header() {
         horizontalArrangement = Arrangement.spacedBy(32.dp, Alignment.CenterHorizontally),
     ) {
         Column(verticalArrangement = Arrangement.Center, modifier = Modifier.align(Alignment.CenterVertically)) {
-            Text("Nathan Reline", fontSize = 24.sp)
-            Text("Android Engineer in Phoenix", modifier = Modifier.alpha(ContentAlpha.medium), fontSize = 20.sp)
+            Text(
+                "Nathan Reline",
+                fontSize = 24.sp,
+                modifier = Modifier.pointerHoverIcon(PointerIcon.Text)
+            )
+            Text(
+                "Android Engineer in Phoenix",
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .alpha(ContentAlpha.medium)
+                    .pointerHoverIcon(PointerIcon.Text),
+            )
         }
         ProfileImage(modifier = Modifier.width(450.dp))
     }
